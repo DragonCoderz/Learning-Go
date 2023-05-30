907a178c36e67b251e1e7ec18a5fdc04fccd26db
Vedic <vedic.panda@gmail.com>
2023-05-30 11:17:25 -0500

    Learned about structs, interfaces, their use cases , as well as table driven tests!

--- StructsMethodsInterf/shapes.go
+++ StructsMethodsInterf/shapes.go
@@ -2,27 +2,44 @@ package shapes
 
 import "math"
 
+type Shape interface {
+	Area() float64
+}
+
+//Automatic interface checking
+
+//Rectangle has a method called Area that returns a float64 so it satisfies the Shape interface
+//Circle has a method called Area that returns a float64 so it satisfies the Shape interface
+
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+//This is a method specific to the rectangle struct
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
 }
 
 type Circle struct {
 	Radius float64
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
-}
-
-// func Area(rectangle Rectangle) float64 {
-// 	return (rectangle.Width * rectangle.Height)
-// }
-
-func (r Rectangle) Area() float64 {
-	return r.Width * r.Height
-}
-
+//This is a method specific to the circle struct
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return t.Base * t.Height * (0.5)
+}
+
+//This is a function
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
 }
--- StructsMethodsInterf/shapes_test.go
+++ StructsMethodsInterf/shapes_test.go
@@ -12,24 +12,49 @@ func TestPerimeter(t *testing.T) {
 	}
 }
 
+// func TestArea(t *testing.T) {
+// 	checkArea := func(t testing.TB, shape Shape, want float64) {
+// 		t.Helper()
+// 		got := shape.Area()
+// 		if got != want {
+// 			t.Errorf("got %.2f want %.2f", got, want)
+// 		}
+// 	}
+
+// 	t.Run("rectangle", func(t *testing.T) {
+// 		rectangle := Rectangle{6.0, 9.0}
+// 		want := 54.0
+// 		checkArea(t, rectangle, want)
+
+// 	})
+
+// 	t.Run("circle", func(t *testing.T) {
+// 		circle := Circle{10}
+// 		want := 314.1592653589793
+// 		checkArea(t, circle, want)
+// 	})
+// }
+
+//Table Driven Test version of TestArea
 func TestArea(t *testing.T) {
-	t.Run("rectangle", func(t *testing.T) {
-		rectangle := Rectangle{6.0, 9.0}
-		got := rectangle.Area()
-		want := 54.0
 
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
-	})
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+	}
 
-	t.Run("circle", func(t *testing.T) {
-		circle := Circle{10}
-		got := circle.Area()
-		want := 314.1592653589793
-
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
-	})
+	for _, tt := range areaTests {
+		// using tt.name from the case to use it as the `t.Run` test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
+	}
 }
